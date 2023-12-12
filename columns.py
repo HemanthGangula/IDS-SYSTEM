5b544ee7f7cfbc0b40bb4546b18a3daa2de25a51
Hemanth <81347482+HemanthGangula@users.noreply.github.com>
2023-12-12 18:02:16 +0530

    Add files via upload

---
+++ columns.py
@@ -0,0 +1,6 @@
+df_columns = ["DestMac", "SrcMac", "EthProtocol", "Version", "TTL", "Proto",
+                  "SrcIP", "DestIP", "ICMPType", "ICMPCode", "TCPSourcePort", "TCPDestPort", "TCPSequence",
+                  "TCPAcknowledgement", "FlagURG", "FlagACK", "FlagPSH", "FlagRST", "FlagSYN", "FlagFIN",
+                  "UDPSrcPort", "UDPDestPort", "UDPLength", "Duration",
+                  "ARP_HW_Type", "ARP_Proto_Type", "ARP_Opcode",
+                  "ARP_Sender_MAC", "ARP_Sender_IP", "ARP_Target_MAC", "ARP_Target_IP"]
